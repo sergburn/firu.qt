b56102fbb095af81fc15c4ba57553a5281cb0554
sergburn <sergburn@497590ae-b53e-4291-bb47-07b2b5baf046>
2009-04-27 11:36:45 +0000

    add sqlite

---
+++ data/createdb.sql
@@ -0,0 +1,53 @@
+BEGIN TRANSACTION;
+CREATE TABLE entries_fi ( id integer primary key autoincrement, text text);
+CREATE TABLE entries_ru ( id integer primary key autoincrement, text text);
+CREATE TABLE trans_fi_ru ( id integer primary key autoincrement, sid integer not null references entries_fi (id) on delete cascade, tid integer not null references entries_ru (id) on delete cascade, fmark integer default 0, rmark integer default 0 );
+CREATE INDEX index_text_fi on entries_fi ( text asc );
+CREATE INDEX index_text_ru on entries_ru ( text asc );
+CREATE TRIGGER genfkey1_insert_referencing BEFORE INSERT ON "trans_fi_ru" WHEN
+    new."tid" IS NOT NULL AND NOT EXISTS (SELECT 1 FROM "entries_ru" WHERE new."tid" == "id")
+BEGIN
+  SELECT RAISE(ABORT, 'constraint failed');
+END;
+CREATE TRIGGER genfkey1_update_referencing BEFORE
+    UPDATE OF tid ON "trans_fi_ru" WHEN
+    new."tid" IS NOT NULL AND
+    NOT EXISTS (SELECT 1 FROM "entries_ru" WHERE new."tid" == "id")
+BEGIN
+  SELECT RAISE(ABORT, 'constraint failed');
+END;
+CREATE TRIGGER genfkey1_delete_referenced BEFORE DELETE ON "entries_ru" WHEN
+    EXISTS (SELECT 1 FROM "trans_fi_ru" WHERE old."id" == "tid")
+BEGIN
+  DELETE FROM "trans_fi_ru" WHERE "tid" = old."id";
+END;
+CREATE TRIGGER genfkey1_update_referenced AFTER
+    UPDATE OF id ON "entries_ru" WHEN
+    EXISTS (SELECT 1 FROM "trans_fi_ru" WHERE old."id" == "tid")
+BEGIN
+  SELECT RAISE(ABORT, 'constraint failed');
+END;
+CREATE TRIGGER genfkey2_insert_referencing BEFORE INSERT ON "trans_fi_ru" WHEN
+    new."sid" IS NOT NULL AND NOT EXISTS (SELECT 1 FROM "entries_fi" WHERE new."sid" == "id")
+BEGIN
+  SELECT RAISE(ABORT, 'constraint failed');
+END;
+CREATE TRIGGER genfkey2_update_referencing BEFORE
+    UPDATE OF sid ON "trans_fi_ru" WHEN
+    new."sid" IS NOT NULL AND
+    NOT EXISTS (SELECT 1 FROM "entries_fi" WHERE new."sid" == "id")
+BEGIN
+  SELECT RAISE(ABORT, 'constraint failed');
+END;
+CREATE TRIGGER genfkey2_delete_referenced BEFORE DELETE ON "entries_fi" WHEN
+    EXISTS (SELECT 1 FROM "trans_fi_ru" WHERE old."id" == "sid")
+BEGIN
+  DELETE FROM "trans_fi_ru" WHERE "sid" = old."id";
+END;
+CREATE TRIGGER genfkey2_update_referenced AFTER
+    UPDATE OF id ON "entries_fi" WHEN
+    EXISTS (SELECT 1 FROM "trans_fi_ru" WHERE old."id" == "sid")
+BEGIN
+  SELECT RAISE(ABORT, 'constraint failed');
+END;
+COMMIT;
--- firuqt.pro
+++ firuqt.pro
@@ -3,23 +3,28 @@ TARGET = firuqt
 QT += core \
     gui
 HEADERS += src/AppUi.h \
-    src/firumainwindow.h \
-    src/firuqt.h \
-    src/trainerdialog.h \
+    src/firumainwindow.h \    
+    src/firuqt.h \    
+    src/trainerdialog.h \    
     src/data.h
-SOURCES += src/AppUi.cpp \
-    src/firumainwindow.cpp \
-    src/main.cpp \
-    src/firuqt.cpp \
-    src/trainerdialog.cpp \
-    src/data.cpp
+SOURCES += src/AppUi.cpp \    
+		src/firumainwindow.cpp \
+		src/main.cpp \
+		src/firuqt.cpp \
+		src/trainerdialog.cpp \
+		src/data.cpp
 FORMS += src/firumainwindow.ui \
     src/firuqt.ui \
     src/trainerdialog.ui
 RESOURCES += 
 symbian: { 
     TARGET.UID3 = 0xE92D4440
-    HEADERS += src/AppUi_S60.h
-    SOURCES += src/firuqt_reg.rss \
-        src/AppUi_S60.cpp
+    HEADERS += firuqt.loc \
+    		src/AppUi_S60.h \
+    		external/sqlite/sqlite3.h
+    SOURCES += firuqt.rss \
+    		firuqt_reg.rss \
+        src/firuqt_reg.rss \
+        src/AppUi_S60.cpp \
+    		external/sqlite/sqlite3.c
 }
