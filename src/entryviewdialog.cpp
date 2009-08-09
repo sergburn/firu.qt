7f8d13b68f54336b63f7e84f5e279668aebf36c8
sergburn <sergburn@497590ae-b53e-4291-bb47-07b2b5baf046>
2009-08-09 19:18:26 +0000

    entry view in single label

--- src/entryviewdialog.cpp
+++ src/entryviewdialog.cpp
@@ -59,26 +59,30 @@ void EntryViewDialog::mousePressEvent( QMouseEvent* /*mouseEvent*/ )
 int EntryViewDialog::exec( const QString& entry, const QStringList& translations )
 {
     m_ui->laEntry->setText( entry );
+    QString trans;
     for ( int i = 0; i < translations.count(); i++ )
     {
         if ( i > 0 )
         {
-            QFrame* line = new QFrame(m_ui->frame);
-            line->setObjectName(QString::fromUtf8("line%1").arg(i));
-            line->setFrameShape(QFrame::HLine);
-            line->setFrameShadow(QFrame::Sunken);
-            m_ui->verticalLayout_3->addWidget(line);
+//            QFrame* line = new QFrame(m_ui->frame);
+//            line->setObjectName(QString::fromUtf8("line%1").arg(i));
+//            line->setFrameShape(QFrame::HLine);
+//            line->setFrameShadow(QFrame::Sunken);
+//            m_ui->verticalLayout_3->addWidget(line);
+            trans.append( "<hr>" );
         }
 
-        QLabel* label = new QLabel(m_ui->frame);
-        label->setObjectName(QString::fromUtf8("label%1").arg(i));
-        label->setWordWrap( true );
-        label->setText( translations[i] );
-        m_ui->verticalLayout_3->addWidget(label);
+//        QLabel* label = new QLabel(m_ui->frame);
+//        label->setObjectName(QString::fromUtf8("label%1").arg(i));
+//        label->setWordWrap( true );
+//        label->setText( translations[i] );
+//        m_ui->verticalLayout_3->addWidget(label);
+        trans.append( translations[i] );
     }
 
-    QSpacerItem* verticalSpacer = new QSpacerItem(0, 0, QSizePolicy::Minimum, QSizePolicy::Expanding);
-    m_ui->verticalLayout_3->addItem(verticalSpacer);
+//    QSpacerItem* verticalSpacer = new QSpacerItem(0, 0, QSizePolicy::Minimum, QSizePolicy::Expanding);
+//    m_ui->verticalLayout_3->addItem(verticalSpacer);
 
+    m_ui->label->setText( trans );
     return QDialog::exec();
 }
